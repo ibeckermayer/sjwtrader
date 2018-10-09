4caa15ebfbfc2e512e8ae0f21b6cdc5018a3c429
ibeckermayer <ibeckermayer@gmail.com>
2018-10-08 22:05:36 -0700

    created skeleton for class to store controversy data

---
+++ prelim_research/model/Controversy.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+class Controversy:
+    """Class for storing controversy data
+
+    @param company (str):       Colloquial name for the company.
+    @param stock   (list[str]): List of relevant stock abbreviations for company.
+    @param date    (datetime):  Date of controversy.
+    @param summary (str):       One line summary of the controversy.
+    @param source: (str):       URL of article confirming controversy and date of the controversy.
+    @param notes:  (str):       Any relevant notes about the company/controversy not contained in the other parameters.
+    """
+    def __init__(self, company, stock, date, summary, source, notes=None):
+        """init for Controversy
+        """
+        self.company = company
+        self.stock = stock
+        self.date = date
+        self.summary = summary
+        self.source = source
+        self.notes = notes if notes is not None else ""
