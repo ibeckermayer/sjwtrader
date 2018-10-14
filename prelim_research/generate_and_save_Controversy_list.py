d31c3537b387a9a999c168d146f4f5225a6c517f
ibeckermayer <ibeckermayer@gmail.com>
2018-10-14 12:47:26 -0700

    added name==main

--- prelim_research/generate_and_save_Controversy_list.py
+++ prelim_research/generate_and_save_Controversy_list.py
@@ -35,16 +35,17 @@ def date_to_datetime(date):
 
     return datetime.strptime(mmddyyyy, "%m/%d/%Y")
 
-df = pd.read_csv(DATA, encoding=ENCODING)
+if __name__ == "__main__":
+    df = pd.read_csv(DATA, encoding=ENCODING)
 
-controversies = []              # list to hold all the controversies
-for index, row in df.iterrows():  # convert each row into Controversy and add to list
-    controversies.append(Controversy(row["Company"],
-                                     row["Stock"].split('-'),
-                                     date_to_datetime(row["Date_Of_Controversy_m/d/y"]),
-                                     row["Summary"],
-                                     row["Source"],
-                                     row["Notes"]))
+    controversies = []              # list to hold all the controversies
+    for index, row in df.iterrows():  # convert each row into Controversy and add to list
+        controversies.append(Controversy(row["Company"],
+                                         row["Stock"].split('-'),
+                                         date_to_datetime(row["Date_Of_Controversy_m/d/y"]),
+                                         row["Summary"],
+                                         row["Source"],
+                                         row["Notes"]))
 
-with open(PIK, "wb") as f:
-    pickle.dump(controversies, f)
+    with open(PIK, "wb") as f:
+        pickle.dump(controversies, f)
