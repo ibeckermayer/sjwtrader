5b8b4d5d57fccae6606c40f099e313060289da7e
ibeckermayer <ibeckermayer@gmail.com>
2018-10-14 22:46:27 -0700

    Oh my GOD that was a lot of work. This commit I would consider the first iteration of the prelimenary report

--- prelim_research/generate_and_save_Controversy_list.py
+++ prelim_research/generate_and_save_Controversy_list.py
@@ -37,15 +37,43 @@ def date_to_datetime(date):
 
 if __name__ == "__main__":
     df = pd.read_csv(DATA, encoding=ENCODING)
+    # df = df[:-1]                # the last row is all NaN so drop it.
 
-    controversies = []              # list to hold all the controversies
+    # load list of controversies if it already exists
+    # @WARNING: if you changed the order of the csv file from the last time
+    #           this program was run, this code will not work properly.
+    #           In that case, delete data/controversies.pickle and reload all data.
+    previous_controversies_loaded = False
+    try:
+        with open(PIK, "rb") as f:
+            controversies = pickle.load(f)
+            previous_controversies_loaded = True
+    except:
+        controversies = []      # list to hold all the controversies
+        pass
+
     for index, row in df.iterrows():  # convert each row into Controversy and add to list
-        controversies.append(Controversy(row["Company"],
-                                         row["Stock"].split('-'),
-                                         date_to_datetime(row["Date_Of_Controversy_m/d/y"]),
-                                         row["Summary"],
-                                         row["Source"],
-                                         row["Notes"]))
+        if  previous_controversies_loaded:
+            if not(index > len(controversies)-1):
+                print("Already loaded, skip")
+            else:
+                controversies.append(Controversy(row["Company"],
+                                                 row["Stock"].split('-'),
+                                                 date_to_datetime(row["Date_Of_Controversy_m/d/y"]),
+                                                 row["Summary"],
+                                                 row["Source"],
+                                                 row["Notes"]))
+        else:
+            print("No previous data")
+            controversies.append(Controversy(row["Company"],
+                                             row["Stock"].split('-'),
+                                             date_to_datetime(row["Date_Of_Controversy_m/d/y"]),
+                                             row["Summary"],
+                                             row["Source"],
+                                             row["Notes"]))
 
     with open(PIK, "wb") as f:
         pickle.dump(controversies, f)
+
+
+        
--- prelim_research/model/Controversy.py
+++ prelim_research/model/Controversy.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+from matplotlib import rcParams
 from alpha_vantage.timeseries import TimeSeries
-from datetime import datetime
+from datetime import datetime, timedelta
 from time import sleep
+
+# Make the dates stay in the scope of the figure
+rcParams.update({'figure.autolayout': True})
 
 with open('ALPHA_VANTAGE_API_KEY', 'r') as f:
     API_KEY = f.read()[:-1]      # get API key, ignore the \n at the end
@@ -11,10 +17,6 @@ with open('ALPHA_VANTAGE_API_KEY', 'r') as f:
 ts = TimeSeries(key=API_KEY, output_format='pandas')    # create instance with API key
 
 # make indexing columns of alpha_vantage response easier
-OPEN   = "1. open"
-HIGH   = "2. high"
-LOW    = "3. low"
-CLOSE  = "4. close"
 ADJ_CLOSE = "5. adjusted close"
 
 class Controversy:
@@ -27,7 +29,7 @@ class Controversy:
     @param[in] source: (str):       URL of article confirming controversy and date of the controversy.
     @param[in] notes:  (str):       Any relevant notes about the company/controversy not contained in the other parameters.
 
-    @param[out] stock_data_dfs:    (list[pd.DataFrame]): List of DataFrames containing the actual stock data corresponding to each
+    @param[out] dfs:    (list[pd.DataFrame]): List of DataFrames containing the actual stock data corresponding to each
                                               stock abbreviation listed in stocks.
     """
 
@@ -42,10 +44,101 @@ class Controversy:
         self.notes = notes if notes is not None else ""
 
         # get all the data on hand for each stock, as pd dataframes
-        self.stock_data_dfs = []
+        self.dfs = []
         print("Fetching stock data for the company {}".format(self.company))
         for stock in self.stocks:
             print("Fetching data for stock {}".format(stock))
-            self.stock_data_dfs.append(ts.get_daily_adjusted(symbol=stock, outputsize='full')[0])
+            self.dfs.append(ts.get_daily_adjusted(symbol=stock, outputsize='full')[0])
             sleep(60)           # sleep a minute to avoid over-calling the API
         print()
+
+    def get_N_day_plot(self, N):
+        """Generates a plot for relevant stock data three days either side of the controversy
+
+        @return (plt.figure) figure containing the plots
+        """
+        # setup figure formatting
+        f = plt.figure()
+        ax = f.add_subplot(1,1,1)
+        ax.xaxis_date()
+        plt.xticks(rotation=70)
+        fmt = '${x:.2f}'
+        tick = mtick.StrMethodFormatter(fmt)
+        ax.yaxis.set_major_formatter(tick)
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Price")
+        for i in range(len(self.dfs)):
+            df = self.dfs[i]
+            df = df[df[ADJ_CLOSE] != 0]  # drop any points where adj_closed == 0, appears to be noise
+            stock = self.stocks[i]
+            date = self.date
+            df = df.reset_index()    # take date index and make it a column, now have normal integer indexing
+            # loop for if the self date is on a weekend
+            # if so, keep going back one day
+            con_index = None
+            delt_days = 0
+            while con_index is None and delt_days < len(df):
+                try:
+                    con_index = df[df["date"] ==
+                                   (date - timedelta(days=delt_days)).strftime("%Y-%m-%d")].index[0]  # find the index corresponding to controversy date
+                except:
+                    delt_days+=1
+                    pass
+            if delt_days == len(df):
+                print("No data for these dates")
+        # for i in range(len(self.dfs)):
+        #     df = self.dfs[i]
+        #     df = df[df[ADJ_CLOSE] != 0]  # drop any points where adj_closed == 0, appears to be noise
+        #     stock = self.stocks[i]
+        #     date = self.date
+        #     df = df.reset_index()    # take date index and make it a column, now have normal integer indexing
+
+        #     # loop for if the controversy date is on a weekend
+        #     # if so, keep going back one day
+        #     con_index = None
+        #     delt_days = 0
+        #     while con_index is None:
+        #         try:
+        #             con_index = df[df["date"] ==
+        #                            (date - timedelta(days=delt_days)).strftime("%Y-%m-%d")].index[0]  # find the index corresponding to controversy date
+        #         except:
+        #             delt_days+=1
+        #             pass
+            three_day_df = df[(con_index-N)-delt_days:(con_index+N+1)+delt_days]  # get the data within N days
+            dates = matplotlib.dates.date2num([datetime.strptime(d, "%Y-%m-%d") for d in three_day_df["date"].values])
+            adjusted_close = three_day_df[ADJ_CLOSE].values
+            ax.plot(matplotlib.dates.date2num(date),
+                    max(adjusted_close), 's',
+                    marker='v', color='r',
+                    label = "Date of Controversy")
+            ax.plot(matplotlib.dates.date2num(date),
+                    min(adjusted_close), 's',
+                    marker='^', color='r')
+            ax.plot([matplotlib.dates.date2num(date)]*2,
+                    [max(adjusted_close), min(adjusted_close)],
+                    marker=',', color='r')
+            ax.plot(dates, adjusted_close, linestyle='-', marker=',', label=stock)
+            plt.title("Stock price {} days either side of controversy".format(N))
+            plt.legend(bbox_to_anchor=(1.04,1), loc="upper left")
+
+        return f
+
+        for i in range(len(controversy.dfs)):
+            df = controversy.dfs[i]
+            df = df[df[ADJ_CLOSE] != 0]  # drop any points where adj_closed == 0, appears to be noise
+            stock = controversy.stocks[i]
+            date = controversy.date
+            df = df.reset_index()    # take date index and make it a column, now have normal integer indexing
+            # loop for if the controversy date is on a weekend
+            # if so, keep going back one day
+            con_index = None
+            delt_days = 0
+            while con_index is None and delt_days < len(df):
+                try:
+                    con_index = df[df["date"] ==
+                                   (date - timedelta(days=delt_days)).strftime("%Y-%m-%d")].index[0]  # find the index corresponding to controversy date
+                except:
+                    delt_days+=1
+                    pass
+            if delt_days == len(df):
+                print("No data for these dates")
